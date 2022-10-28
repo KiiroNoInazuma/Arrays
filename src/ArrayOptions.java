2d2a8e2ef9f02e18d8dcb5ad4f0a208782d0877b
John Rey <adioskid@mail.ru>
2022-10-29 00:00:49 +0300

    Homework#4

--- src/ArrayOptions.java
+++ src/ArrayOptions.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class ArrayOptions {
     public static void main(String[] args) {
-
+        int[] arr = new int[16];
+        int a = 1;
+        for (int x = 1; x < arr.length; x++) {
+            a *= 2;
+            arr[x] = a;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
