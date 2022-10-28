a4f4acb235eeb3ecfea90fdebd5031c8d4c83d9e
John Rey <adioskid@mail.ru>
2022-10-29 00:22:56 +0300

    Homework#4

--- src/ArrayOptions.java
+++ src/ArrayOptions.java
@@ -7,12 +7,22 @@ public class ArrayOptions {
         for (int x = 1; x < arr.length; x++) {
             a *= 2;
             arr[x] = a;
-            if(x==arr.length-1){
-                System.out.print(x);
-            }else {
-                System.out.print(x+",");
+            if (x == arr.length - 1) {
+                System.out.print(arr[x]);
+            } else {
+                System.out.print(arr[x] + ",");
+            }
+
+        }
+        System.out.println();
+        for (int z = arr.length - 1; z >= 0; z--) {
+            if (z == 0) {
+                System.out.print(arr[z]);
+            } else {
+                System.out.print(arr[z] + ",");
             }
         }
-        System.out.println("\n"+Arrays.toString(arr));
+
+        System.out.println("\n" + Arrays.toString(arr));
     }
 }
