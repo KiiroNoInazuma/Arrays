f3c6dc88734f49f95b53ef07e831fa7dd515b846
John Rey <adioskid@mail.ru>
2022-11-03 23:25:13 +0300

    Homework#4

--- src/FibonacciNumbers.java
+++ src/FibonacciNumbers.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class FibonacciNumbers {
     public static void main(String[] args) {
-
+        int[] fibonacci = new int[20];
+        for (int x = 0; x < fibonacci.length; x++) {
+            if (x < 2) {
+                fibonacci[x] = x;
+            } else {
+                fibonacci[x] = fibonacci[x - 1] + fibonacci[x - 2];
+            }
+        }
+        System.out.println(Arrays.toString(fibonacci));
     }
 }
